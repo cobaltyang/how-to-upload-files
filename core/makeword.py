7088b99a0adcc76f7b62e9e0fcc0f3c3d83e896c
geekyangshuo <784547879@qq.com>
2021-05-07 20:58:03 +0800

    2021年 5月7日完成 666

--- core/makeword.py
+++ core/makeword.py
@@ -1,3 +1,4 @@
+import pythoncom
 from docx2pdf import convert
 from docxtpl import DocxTemplate, Subdoc
 from datetime import datetime
@@ -5,6 +6,7 @@ from docx import Document
 from reportlab.pdfgen import canvas
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 import os,shutil
+import win32com.client as win32
 from .zhengwen import zhengwen_fix
 
 def zhuanhuan(filename):
@@ -26,17 +28,42 @@ def add_dict(context,sum_dict):
 
     return sum_dict
 
+
+def buildcover(context):  #制作封面
+    doc = DocxTemplate('static/docx/covermoban.docx')  # 加载模板文件
+    doc.render(context) #填充数据
+    doc.save('static/pdfresult/head.docx') #保存目标文件
+
+
+def buildend(content):  #制作后边
+    doc = DocxTemplate('static/docx/endmoban.docx')  # 加载模板文件
+    print('这里执行了')
+    doc.render(content) #填充数据
+    doc.save('static/pdfresult/tail.docx') #保存目标文件
+
 def chongmingming(srcpath,dstpath):
     os.rename(srcpath,dstpath)
     print("重命名完毕")
 
 # 多输入实现方式
-def final(content,outputname,up_name):
-    doc = DocxTemplate('static/docx/moban.docx')  # 加载模板文件
-    sd = doc.new_subdoc('static/pdfresult/' + up_name)#用户上传的子文档
-    content['mysubdoc'] = sd
-    doc.render(content) #填充数据
-    doc.save('static/pdfresult/'+outputname+'.docx') #保存目标文件
+def final(up_name):
+
+    pythoncom.CoInitialize()
+    word = win32.gencache.EnsureDispatch('Word.Application')
+    # 非可视化运行
+    word.Visible = False
+    output = word.Documents.Add()  # 新建合并后空白文档
+    # 需要合并的文档路径，这里有个文档1.docx，2.docx，3.docx.
+    # files = ['E://newcode//one//3.docx', 'E://newcode//one//2.docx', 'E://newcode//one//1.docx']
+    files = ['E://newcode//untitled6//static//pdfresult//tail.docx','E://newcode//untitled6//static//pdfresult//'+up_name, 'E://newcode//untitled6//static//pdfresult//head.docx']
+    for file in files:
+        output.Application.Selection.Range.InsertFile(file)  # 拼接文
+
+    output.SaveAs('E://newcode//untitled6//static//pdffinal//final.docx')  # 保存
+    output.Close()
+
+
+
 
 
 
--- core/views.py
+++ core/views.py
@@ -5,14 +5,15 @@ from django.core.cache import cache
 from .zhengwen import zhengwen_fix
 from django.http import HttpResponse, FileResponse
 from django.shortcuts import render
-from .forms import AuthorForm,AfterForm,AbstractForm
+from .forms import AuthorForm, AfterForm, AbstractForm
 import os
-from .makeword import add_dict, final
+from .makeword import add_dict, final, buildend, buildcover
 
 from reportlab.pdfgen import canvas
 from docx2pdf import convert
-# Create your views here.
 
+
+# Create your views here.
 
 
 def index(request):
@@ -21,51 +22,57 @@ def index(request):
         abstract_form = AbstractForm()
         if author_form.is_valid():
             sum_dict = {}
-            dict_result1 = add_dict(dict(request.POST),sum_dict)
-            cache.set('dict',dict_result1)
-            cache.set('filename',request.POST['outputname'])
-
+            dict_result1 = add_dict(dict(request.POST), sum_dict)
+            cache.set('dict0', dict_result1)
+            cache.set('filename', request.POST['outputname'])
 
             print(dict_result1)
 
             author_form.save()
 
-            return render(request, 'core/abstract.html', {"abstract_form":abstract_form, })
+            return render(request, 'core/abstract.html', {"abstract_form": abstract_form, })
         else:
-            return render(request,'core/error.html',{'form':author_form})
+            return render(request, 'core/error.html', {'form': author_form})
     else:
         author_form = AuthorForm(request.POST)
         return render(request, "core/index.html", {"form": author_form})
+
 
 def after(request):
     if request.method == "POST":
         after_form = AfterForm()
         abstract_form = AbstractForm(request.POST, request.FILES)
-        if  abstract_form.is_valid():
+        if abstract_form.is_valid():
             abstract_form.save()
-            dict_second =  cache.get('dict')
-            dict_result2 =  add_dict(dict(request.POST), dict_second)
-            cache.set('dict',dict_result2)
+            dict_second = cache.get('dict0')
+            dict_result2 = add_dict(dict(request.POST), dict_second)
+            cache.set('dict0', dict_result2)
             cache.set('up_name', request.FILES['wendang'].name)
+            buildcover(dict_result2)
 
             print(dict_result2)
             return render(request, "core/after.html", {"after_form": after_form})
         else:
-            return  render(request, "core/download.html", )
+            return render(request, "core/download.html", )
 
-#基本成功
+
+# 基本成功
 def download_pdf(request):
-    dict_third = cache.get('dict')
-    dict_result3 = add_dict(dict(request.POST), dict_third)
-    cache.set('dict', dict_result3)
+    sumdict = {}
+    dict_result3 = add_dict(dict(request.POST), sumdict)
+    cache.set('dict1', dict_result3)
+
+    buildend(dict_result3)
     print(dict_result3)
+    print('这是dic——result4')
 
     name = cache.get('filename')
     up_name = cache.get('up_name')
-    zhengwen_fix(up_name)
-    print(name)
-    final(dict_result3,name,up_name)
-    file = open('static/pdfresult/'+name+'.docx', 'rb')
+    zhengwen_fix(up_name)  #
+
+    final(up_name)
+
+    file = open('static/pdffinal/final.docx', 'rb')
     response = FileResponse(file)
     response['Content-Type'] = 'application/octet-stream'
     response['Content-Disposition'] = 'attachment;filename =result.docx'
